49f31e3066d29c5bc9f6e1b680229b135a1c511a
Brian Ott <brian.ott@de.airbus.com>
2023-12-19 14:42:44 +0100

    outlining script; importing libs

---
+++ cisco_ssh_connect.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import csv
+import sys
+import re
+import netmiko
+from datetime import datetime
+
+def main():
+    switch = input("What's the hostname of the switch? ")
+
+    pass
+
+if __name__ == '__main__':
+    main()
